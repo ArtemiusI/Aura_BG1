94adbb69216318fc9f986367104c15b7d65c3a68
ArtemiusI <whuangx10@gmail.com>
2025-02-07 00:46:33 +0800

    v4.1
    Kit modifications: Infusion now only adds enchantment to chosen weapon type, healing arrows have reduced value, elemental infusions now increase resistance

--- Aura_BG1/artificer/dialogue/c0aualch.d
+++ Aura_BG1/artificer/dialogue/c0aualch.d
@@ -5,7 +5,8 @@ SAY ~What do you wish to create?~
 + ~!Global("C0AUIN5","GLOBAL",1)~ + ~Potions~ DO ~SetGlobal("C0ArtificerAlchemy","LOCALS",1) StartStore("c0aupot1",Player1)~ EXIT
 + ~Global("C0AUIN5","GLOBAL",1)~ + ~Standard Potions~ DO ~SetGlobal("C0ArtificerAlchemy","LOCALS",1) StartStore("c0aupot1",Player1)~ EXIT
 + ~Global("C0AUIN5","GLOBAL",1) Global("C0AdvancedAlchemy","LOCALS",0)~ + ~Advanced Potions~ DO ~SetGlobal("C0ArtificerAlchemy","LOCALS",1) StartStore("c0aupoth",Player1)~ EXIT
-+ ~OR(2)
++ ~OR(3)
 GlobalGT("Chapter","GLOBAL",7)
-GlobalGT("C0AuraQuest","GLOBAL",8)~ + ~Explosives~ DO ~SetGlobal("C0ArtificerAlchemy","LOCALS",1) StartStore("c0aubom1",Player1)~ EXIT
+GlobalGT("C0AuraQuest","GLOBAL",8)
+GlobalGT("C0AuraAlchemyBG2","GLOBAL",0)~ + ~Explosives~ DO ~SetGlobal("C0ArtificerAlchemy","LOCALS",1) StartStore("c0aubom1",Player1)~ EXIT
 END
